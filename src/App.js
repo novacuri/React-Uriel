584cdfc47ce3698068189464dc393862aafe85db
Uriel Novaczyk <novacuri2@hotmail.com>
2022-12-10 14:13:46 -0300

    Segunda Entrega

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import ItemListContainer from "./componentes/ItemListContainer/ItemListContainer"
 import NavBar from './componentes/NavBar/NavBar'; 
-
+import ClickCount from "..src/componentes/ClickCount/ClickCount" 
 
 function App() {
   return (
@@ -12,4 +12,13 @@ function App() {
   );
 }
 
-export default App;
+/* export default App; */
+
+export default function App()
+return(
+  <div>
+  <h1>"Elegi tu bicicleta"</h1>
+  <ClickCount stock="6"/>
+  </div>
+)
+
